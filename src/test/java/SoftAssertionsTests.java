18c5e9817004bcc5476014665c576daeab95d015
Mukhammadjon Pulatbekov <mukhammadjonpulatbekov@gmail.com>
2024-06-12 22:40:34 +0500

    HW4

---
+++ src/test/java/SoftAssertionsTests.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+
+
+public class SoftAssertionsTests {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.browserSize = "1920x1080";
+    }
+    @Test
+    void pageHasJUnit5Test(){
+        open("selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-body").shouldHave(text("Soft assertions"));
+        $(byText("Soft assertions")).click();
+        $("#wiki-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }"""));
+    }
+}
