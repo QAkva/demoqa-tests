0a9f4ccfeeedcce4afdfb742d26b8e61c1fe51ad
Mukhammadjon Pulatbekov <mukhammadjonpulatbekov@gmail.com>
2024-06-14 22:19:56 +0500

    hover test added

---
+++ src/test/java/NavigationBarTests.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class NavigationBarTests {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.browserSize = "1920x1080";
+    }
+    @Test
+    void hoverTest(){
+        open("");
+        $(byTagAndText("button", "Solutions")).hover();
+        $(byTagAndText("a", "Enterprise")).click();
+        $(byText("The AI-powered")).shouldBe(visible);
+        }
+
+    }
